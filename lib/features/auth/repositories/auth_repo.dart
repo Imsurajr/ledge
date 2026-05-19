a7f163a8dcb49e375e9619e9fc67cecde953f8ba
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-05-19 12:28:18 +0530

    furthermore refactoring authentication repo with typedef for ResultFuture

--- lib/features/auth/repositories/auth_repo.dart
+++ lib/features/auth/repositories/auth_repo.dart
@@ -3,11 +3,11 @@ import '../domain/entities/user.dart';
 
 abstract class AuthRepo {
   const AuthRepo();
-  Future<ResultFuture<void>> createUser({
+  ResultFuture<void> createUser({
     required String createdAt,
     required String name,
     required String avatar,
   });
 
-  Future<ResultFuture<List<User>>> getUsers();
+  ResultFuture<List<User>> getUsers();
 }
