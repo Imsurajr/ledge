425ec7557532dea00d3b836bea9e21a81e8cf6cd
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-06-02 10:34:58 +0530

    using constant usecase for create user usecase

--- lib/features/auth/domain/usecases/create_user.dart
+++ lib/features/auth/domain/usecases/create_user.dart
@@ -1,6 +1,11 @@
+import 'package:equatable/equatable.dart';
+import 'package:ledge/core/usecase/usecase.dart';
 import 'package:ledge/features/auth/domain/repositories/auth_repo.dart';
 import 'package:ledge/utils/typedef.dart';
 
+/// now we are using the custom Usecase which we defined, we are moving from this
+
+/*
 class CreateUser {
   const CreateUser(this._repo);
   final AuthRepo _repo;
@@ -12,4 +17,34 @@ class CreateUser {
     required String avatar,
   }) async =>
       _repo.createUser(createdAt: createdAt, name: name, avatar: avatar);
+}
+ */
+
+/// to this
+class CreateUser extends UsecaseWithParams<void, CreateUserParams> {
+  // passing custom params class as there are more than one params
+  const CreateUser(this._repo);
+  final AuthRepo _repo;
+
+  @override
+  ResultVoid call (CreateUserParams params) async => _repo.createUser(
+    createdAt: params.createdAt,
+    name: params.name,
+    avatar: params.avatar,
+  );
+}
+
+/// this class will be used to define all the params that we want to pass to the usecase
+class CreateUserParams extends Equatable {
+  const CreateUserParams({
+    required this.createdAt,
+    required this.name,
+    required this.avatar,
+  });
+  final String createdAt;
+  final String name;
+  final String avatar;
+
+  @override
+  List<Object?> get props => [createdAt, name, avatar];
 }
