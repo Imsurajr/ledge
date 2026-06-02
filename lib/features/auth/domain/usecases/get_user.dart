c5c6675e878f97f6cd52561027aaf2c585c3ed95
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-06-02 10:41:36 +0530

    get user usecase using the common usecase

---
+++ lib/features/auth/domain/usecases/get_user.dart
@@ -0,0 +1,12 @@
+import 'package:ledge/core/usecase/usecase.dart';
+import 'package:ledge/features/auth/domain/repositories/auth_repo.dart';
+import '../../../../utils/typedef.dart';
+import '../entities/user.dart';
+
+class GetUser extends UsecaseWithoutParams<List<User>> {
+  const GetUser(this._repo);
+  final AuthRepo _repo;
+
+  @override
+  ResultFuture<List<User>> call() async => _repo.getUsers();
+}
