d33850b5a4e781a568ab03bc62edfdff40ed4930
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-06-02 10:44:28 +0530

    documentation

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,5 +1,6 @@
 import '../../utils/typedef.dart';
 
+/// in tdd we are supposed to write usecases for each usecase separately
 abstract class UsecaseWithParams<Type, Params> {
   const UsecaseWithParams();
   ResultFuture<Type> call(Params params);
