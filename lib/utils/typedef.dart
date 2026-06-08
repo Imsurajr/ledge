30b6ff9c31b9b2176a320bbd2b94bff9c3374abe
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-06-08 18:18:13 +0530

    created reusable typedef for DataMap

--- lib/utils/typedef.dart
+++ lib/utils/typedef.dart
@@ -3,3 +3,4 @@ import 'package:ledge/core/errors/failure.dart';
 
 typedef ResultFuture<T> = Future<Either<Failure, T>>; // creating a typedef with type Future with type of Either a Failure or T which is user defined data type
 typedef ResultVoid = ResultFuture<void>; // customizing ResultFuture for user defined data type of void
+typedef DataMap = Map<String, dynamic>; // reusable for map which could be used models
