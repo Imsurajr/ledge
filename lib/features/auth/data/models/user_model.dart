cc5ef04a8f68f249c09293aeb30fd50f9087d489
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-06-08 18:19:10 +0530

    created data/user_model

---
+++ lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:ledge/features/auth/domain/entities/user.dart';
+import 'package:ledge/utils/typedef.dart';
+
+/*
+    first we are supposed to extend the equity then we are supposed to call parameters through super extended class
+    then we are supposed to write from/to json methods or from/to map
+ */
+    // A data model for [User] that handles JSON serialization and deserialization.
+    class UserModel extends User {
+      const UserModel({
+        required super.avatar,
+        required super.createdAt,
+        required super.id,
+        required super.name,
+      });
+
+      // Constructs a [UserModel] from a JSON string by delegating to [fromMap], decode data that comes in json format and pass it as Map
+      factory UserModel.fromJson(String source) =>
+          UserModel.fromMap(jsonDecode(source) as DataMap);
+
+      // Constructs a [UserModel] from a raw key-value map.
+      UserModel.fromMap(DataMap map)
+        : this(
+            createdAt: map['createdAt'] as String,
+            avatar: map['avatar'] as String,
+            name: map['name'] as String,
+            id: map['id'] as String,
+          );
+
+      // Converts this [UserModel] to a [DataMap].
+      DataMap toMap() => {
+        'createdAt': createdAt,
+        'name': name,
+        'avatar': avatar,
+        'id': id,
+      };
+
+      // Serializes this [UserModel] to a JSON string.
+      String toJson() => jsonEncode(toMap());
+    }
