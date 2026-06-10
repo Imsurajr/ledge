e6859942d52c50d40fac78e7652ec38176ab971b
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-06-10 12:32:02 +0530

    added all tests required for User Model

--- test/features/auth/data/models/user_model_test.dart
+++ test/features/auth/data/models/user_model_test.dart
@@ -12,7 +12,6 @@ import '../../../../fixtures/fixture_reader.dart';
 const tModel = UserModel.empty();
 
 void main() {
-
   // 1st test for model
   test('Should be a subclass of [User] entity', () {
     /// Arrange - tModel
@@ -27,19 +26,66 @@ void main() {
   final tMap = jsonDecode(tJson) as DataMap;
 
   group('fromMap', () {
-    test('Should return a [UserModel] with correct data', () {
+    test('Should return a [UserModel] with correct map data', () {
       /// Act
       final res = UserModel.fromMap(tMap);
+
+      /// Assert
+      expect(res, equals(tModel));
+    });
+  });
+
+  // now test for fromJson as a group
+  group('fromJson', () {
+    test('Should return a [UserModel] with correct json data', () {
+      /// Act
+      final res = UserModel.fromJson(tJson);
+
+      /// Assert
       expect(res, equals(tModel));
     });
   });
 
   // now test for toMap as a group
-  group('fromJson', () {
-    test('Should return a [UserModel] with correct data', () {
+  group('toMap', () {
+    test('Should return a [DataMap] with map correct data', () {
       /// Act
-      final res = UserModel.fromJson(tJson);
-      expect(res, equals(tModel));
+      final res = tModel.toMap();
+
+      /// Assert
+      expect(res, equals(tMap));
+    });
+  });
+
+  // now test for toJson as a group
+  group('toJson', () {
+    test('Should return a [JSON] with correct json data', () {
+      /// Act
+      final res = tModel.toJson();
+      final tJson = jsonEncode({
+        "createdAt": "_empty.createdAt",
+        "name": "_empty.name",
+        "avatar": "_empty.avatar",
+        "id": "1",
+      });
+
+      /// Assert
+      expect(res, equals(tJson));
+    });
+  });
+
+  // now test for copyWith as a group
+  group('copyWith', () {
+    test('Should always return [UserModel] with changed data', () {
+      /// Act
+      final res = tModel.copyWith(name: 'Suraj', id: '2', createdAt: '12:30 pm', avatar: 'test_avatar');
+
+      /// Assert
+      expect(res.name, equals('Suraj'));
+      expect(res.avatar, equals('test_avatar'));
+      expect(res.id, equals('2'));
+      expect(res.createdAt, equals('12:30 pm'));
+      // expect(res, equals(tModel));
     });
   });
 }
