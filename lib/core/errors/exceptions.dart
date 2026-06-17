5bb504f948179cb883f24ae87d9d967aadd33cd1
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-06-17 13:04:31 +0530

    documentation for ServerFailure

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -1,5 +1,8 @@
 import 'package:equatable/equatable.dart';
 
+/// we will now define a Server Failure which is extending equatable for comparison with Exceptions threw
+/// by server and will be implementing Exception again to handle Server's threw Exceptions
+
 class ServerFailure extends Equatable implements Exception {
   const ServerFailure({required this.message, required this.statusCode});
 
@@ -8,4 +11,5 @@ class ServerFailure extends Equatable implements Exception {
 
   @override
   List<Object?> get props => [message, statusCode];
+  /// Making sure both message and statusCode matches to throw a Server exception
 }
