e9f0646f2fa96c34a6f17b6556a2290c28f9b58e
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-06-19 15:59:26 +0530

    dedicated Server Exception updated

--- lib/core/errors/exceptions.dart
+++ lib/core/errors/exceptions.dart
@@ -1,10 +1,10 @@
 import 'package:equatable/equatable.dart';
 
-/// we will now define a Server Failure which is extending equatable for comparison with Exceptions threw
+/// we will now define a Server Exception which is extending equatable for comparison with Exceptions threw
 /// by server and will be implementing Exception again to handle Server's threw Exceptions
 
-class ServerFailure extends Equatable implements Exception {
-  const ServerFailure({required this.message, required this.statusCode});
+class ServerException extends Equatable implements Exception {
+  const ServerException({required this.message, required this.statusCode});
 
   final String message;
   final int statusCode;
