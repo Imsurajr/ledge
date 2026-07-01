01777f630b7adb53e64133bb6aadd66e109bd059
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-07-01 18:32:08 +0530

    initialized dependency injection in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:ledge/core/services/injection_container.dart';
 import 'package:logger/logger.dart';
 
-void main() async {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await init();
   Logger logger = Logger();
   try {
     await dotenv.load(fileName: ".env");
