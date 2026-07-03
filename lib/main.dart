4753a1408bf218aa3d8ed817b87c37e032d6e427
imsurajr <mailsurajraghuvanshihere@gmail.com>
2026-07-03 13:04:34 +0530

    app loaded

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart' show BlocProvider;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:ledge/core/services/injection_container.dart';
+import 'package:ledge/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:ledge/features/auth/presentation/views/homescreen.dart';
 import 'package:logger/logger.dart';
 
 Future<void> main() async {
@@ -20,12 +23,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return BlocProvider(
+      /// generally we would inject bloc like this but since we've registered the dependency we wil use sl
+      // create: (context) => AuthCubit(createUser: createUser, getUsers: getUsers),
+      create: (context) => sl<AuthCubit>(),
+      child: MaterialApp(
+        theme: ThemeData(
+          useMaterial3: true,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+        home: const Homescreen(),
       ),
     );
   }
 }
+
